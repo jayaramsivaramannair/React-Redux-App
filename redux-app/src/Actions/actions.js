b62597d363d3b4d7b3d05ae87d17833fc95c8fef
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-03-02 19:38:23 -0500

    Created a random number function to pass the subsequent result to the axios call to enable generation of new random character

--- redux-app/src/Actions/actions.js
+++ redux-app/src/Actions/actions.js
@@ -4,10 +4,14 @@ export const FETCH_REQUEST_PROGRESS = 'FETCH_REQUEST_PROGRESS';
 export const FETCH_REQUEST_SUCCESS = 'FETCH_REQUEST_SUCCESS';
 export const FETCH_REQUEST_FAILURE = 'FETCH_REQUEST_FAILURE';
 
+const getRandomInt = () => {
+    return Math.round(Math.random() * Math.floor(671));
+}
+
 export const getCharacter = () => (dispatch) => {
     dispatch({ type: FETCH_REQUEST_PROGRESS })
 
-    axios.get('https://rickandmortyapi.com/api/character/18')
+    axios.get(`https://rickandmortyapi.com/api/character/${getRandomInt()}`)
         .then((res) => {
             console.log(res.data);
             dispatch({ type: FETCH_REQUEST_SUCCESS, payload: res.data })
